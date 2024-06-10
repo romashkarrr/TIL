7cab1faab10ab2188be8b5600f50699fb6fa2676
romashkarrr <r0man.kish999@gmail.com>
2024-06-10 17:02:59 +0300

    Initial Commit

---
+++ TIL/TILApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILApp.swift
+//  TIL
+//
+//  Created by romashka on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct TILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
